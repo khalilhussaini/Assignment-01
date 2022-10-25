7be88f6fb2994edbf99e98a508f1493a80a89d56
khalil Hussaini <107269671+khalilhussaini@users.noreply.github.com>
2022-10-25 17:50:44 +0200

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,10 @@
+/* var month = ("December");
+var day = 29;
+if (month=="december" && day <30); */
+
+var month = 12; // December
+ var day = 29 ;
+if (month=="december" && day <30);
+console.log("day, month"); // last day in December
+
+  
